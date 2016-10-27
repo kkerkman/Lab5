f4b4bd3da40e00e8b395e75e141ed3a7a866f87c
YOUR NAME <kkerkman@yahoo.com>
2016-10-27 16:05:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+/*function temperature(fahrenheit)
+{
+
+var celsius = ((fahrenheit -32)*(5/9));
+var message = celsius + " degrees celsius"
+console.log(message);
+}
+
+temperature(85); */
+var fahrenheit = 35
+var celsius = ((fahrenheit - 32) * (5/9));
+
+var message = celsius + " is the temperature in celsius";
+console.log(message);
+
+
+
